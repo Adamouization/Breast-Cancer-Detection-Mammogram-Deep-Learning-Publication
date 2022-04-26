f984f8830cd84f466c68d4994268100f57c24b49
Craig <cggm1@st-andrews.ac.uk>
2022-04-26 14:42:39 +0100

    Fix runtime var error

--- src/data_operations/dataset_feed.py
+++ src/data_operations/dataset_feed.py
@@ -54,7 +54,8 @@ def parse_function(filename, label):
         width = config.MOBILE_NET_IMG_SIZE["WIDTH"]
     elif config.model == "CNN":
         pass
-    image = tf.image.resize_with_pad(decoded_png, height, width)  # or 'tf.image.resize'
+    image = tf.image.resize_with_pad(decoded_png, 512, 512)  # or 'tf.image.resize'
+    # image = tf.image.resize_with_pad(decoded_png, height, width)  # or 'tf.image.resize'
     image /= 255
 
     return image, label
